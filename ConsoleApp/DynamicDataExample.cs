6d559e6d4c9c419b523f58db8e1415b0ef976de1
Sergey Shishkin <sergei.shishkin@gmail.com>
2010-07-01 12:44:06 +0200

    all classes are scoped to the containg example

--- ConsoleApp/DynamicDataExample.cs
+++ ConsoleApp/DynamicDataExample.cs
@@ -4,136 +4,136 @@
     using System.Collections.Generic;
     using System.Dynamic;
 
-    /// <summary>
-    /// Generic property bag implementation of an entity.
-    /// </summary>
-    public class Entity : DynamicObject
-    {
-        private readonly Dictionary<string, object> data = new Dictionary<string, object>();
-
-        public Entity(string id)
-        {
-            Id = id;
-        }
-
-        public string Id { get; private set; }
-
-        public override bool TryGetMember(GetMemberBinder binder, out object result)
-        {
-            return data.TryGetValue(binder.Name, out result);
-        }
-
-        public override bool TrySetMember(SetMemberBinder binder, object value)
-        {
-            data[binder.Name] = value;
-            return true;
-        }
-    }
-
-    /// <summary>
-    /// Base class for roles.
-    /// </summary>
-    public abstract class Role
-    {
-        public Role(Entity entity)
-        {
-            Entity = entity;
-        }
-
-        protected dynamic Entity { get; private set; }
-
-        public string Id
-        {
-            get { return Entity.Id; }
-        }
-    }
-
-    /// <summary>
-    /// Context.
-    /// </summary>
-    public class TransferMoney
-    {
-        public SourceAccount Source { get; set; }
-        public DestinationAccount Destination { get; set; }
-        public decimal Amount { get; set; }
-
-        /// <summary>
-        /// Use-case algorithm.
-        /// </summary>
-        public void Execute()
-        {
-            if (Source.Balance < Amount)
-            {
-                throw new ApplicationException("Insufficient funds.");
-            }
-
-            Source.DecreseBalanceBy(Amount);
-            Destination.IncreaseBalanceBy(Amount);
-        }
-
-        /// <summary>
-        /// Role.
-        /// </summary>
-        public class SourceAccount : Role
-        {
-            public SourceAccount(Entity entity) : base(entity) { }
-
-            public decimal Balance
-            {
-                get { return Entity.Balance; }
-            }
-
-            public void DecreseBalanceBy(decimal amount)
-            {
-                Entity.Balance -= amount;
-            }
-        }
-
-        /// <summary>
-        /// Role.
-        /// </summary>
-        public class DestinationAccount : Role
-        {
-            public DestinationAccount(Entity entity) : base(entity) { }
-
-            public void IncreaseBalanceBy(decimal amount)
-            {
-                Entity.Balance += amount;
-            }
-        }
-    }
-
-    /// <summary>
-    /// Context.
-    /// </summary>
-    public class CheckBalance
-    {
-        public Account TheAccount { get; set; }
-
-        /// <summary>
-        /// Use-case algorithm.
-        /// </summary>
-        public void Execute()
-        {
-            Console.WriteLine("{0}: {1:c}", TheAccount.Id, TheAccount.Balance);
-        }
-
-        /// <summary>
-        /// Role.
-        /// </summary>
-        public class Account : Role
-        {
-            public Account(Entity entity) : base(entity) { }
-
-            public decimal Balance
-            {
-                get { return Entity.Balance; }
-            }
-        }
-    }
-
     public static class DynamicDataExample
     {
+        /// <summary>
+        /// Generic property bag implementation of an entity.
+        /// </summary>
+        public class Entity : DynamicObject
+        {
+            private readonly Dictionary<string, object> data = new Dictionary<string, object>();
+
+            public Entity(string id)
+            {
+                Id = id;
+            }
+
+            public string Id { get; private set; }
+
+            public override bool TryGetMember(GetMemberBinder binder, out object result)
+            {
+                return data.TryGetValue(binder.Name, out result);
+            }
+
+            public override bool TrySetMember(SetMemberBinder binder, object value)
+            {
+                data[binder.Name] = value;
+                return true;
+            }
+        }
+
+        /// <summary>
+        /// Base class for roles.
+        /// </summary>
+        public abstract class Role
+        {
+            public Role(Entity entity)
+            {
+                Entity = entity;
+            }
+
+            protected dynamic Entity { get; private set; }
+
+            public string Id
+            {
+                get { return Entity.Id; }
+            }
+        }
+
+        /// <summary>
+        /// Context.
+        /// </summary>
+        public class TransferMoney
+        {
+            public SourceAccount Source { get; set; }
+            public DestinationAccount Destination { get; set; }
+            public decimal Amount { get; set; }
+
+            /// <summary>
+            /// Use-case algorithm.
+            /// </summary>
+            public void Execute()
+            {
+                if (Source.Balance < Amount)
+                {
+                    throw new ApplicationException("Insufficient funds.");
+                }
+
+                Source.DecreseBalanceBy(Amount);
+                Destination.IncreaseBalanceBy(Amount);
+            }
+
+            /// <summary>
+            /// Role.
+            /// </summary>
+            public class SourceAccount : Role
+            {
+                public SourceAccount(Entity entity) : base(entity) { }
+
+                public decimal Balance
+                {
+                    get { return Entity.Balance; }
+                }
+
+                public void DecreseBalanceBy(decimal amount)
+                {
+                    Entity.Balance -= amount;
+                }
+            }
+
+            /// <summary>
+            /// Role.
+            /// </summary>
+            public class DestinationAccount : Role
+            {
+                public DestinationAccount(Entity entity) : base(entity) { }
+
+                public void IncreaseBalanceBy(decimal amount)
+                {
+                    Entity.Balance += amount;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Context.
+        /// </summary>
+        public class CheckBalance
+        {
+            public Account TheAccount { get; set; }
+
+            /// <summary>
+            /// Use-case algorithm.
+            /// </summary>
+            public void Execute()
+            {
+                Console.WriteLine("{0}: {1:c}", TheAccount.Id, TheAccount.Balance);
+            }
+
+            /// <summary>
+            /// Role.
+            /// </summary>
+            public class Account : Role
+            {
+                public Account(Entity entity) : base(entity) { }
+
+                public decimal Balance
+                {
+                    get { return Entity.Balance; }
+                }
+            }
+        }
+
         public static void Run()
         {
             // Create entities and pre-initialize data
--- ConsoleApp/StaticRolesExample.cs
+++ ConsoleApp/StaticRolesExample.cs
@@ -5,149 +5,149 @@ namespace DataContextInteraction
 {
     using System.Collections.Generic;
 
-    /// <summary>
-    /// The only class of the "composition framework".
-    /// </summary>
-    public abstract class RoleFor<T>
-    {
-        public T This { protected get; set; }
-    }
-
-    /// <summary>
-    /// Dumb data: value object.
-    /// </summary>
-    public class AccountOperation
-    {
-        public AccountOperation(decimal amount, string description)
-        {
-            Amount = amount;
-            Description = description;
-        }
-
-        public decimal Amount { get; private set; }
-
-        public string Description { get; private set; }
-    }
-
-    /// <summary>
-    /// Dumb data: entity.
-    /// </summary>
-    public class Account
-    {
-        private readonly List<AccountOperation> operations = new List<AccountOperation>();
-
-        public Account(string number)
-        {
-            Number = number;
-        }
-
-        public string Number { get; private set; }
-
-        public IEnumerable<AccountOperation> Operations
-        {
-            get { return operations.AsEnumerable(); }
-        }
-
-        public void AddOperation(AccountOperation operation)
-        {
-            operations.Add(operation);
-            Console.WriteLine("{0}: {1:c} {2}",
-                Number,
-                operation.Amount,
-                operation.Description);
-        }
-    }
-
-    /// <summary>
-    /// "Intermediate" role for code reuse.
-    /// </summary>
-    public class AccountWithBalance : RoleFor<Account>
-    {
-        public decimal Balance
-        {
-            get { return This.Operations.Sum(x => x.Amount); }
-        }
-    }
-
-    /// <summary>
-    /// Role of the MoneyTransfer context.
-    /// </summary>
-    public class SourceAccount : AccountWithBalance
-    {
-        public void TransferTo(decimal amount, DestinationAccount destination)
-        {
-            if (Balance < amount)
-            {
-                throw new ApplicationException("Insufficient funds.");
-            }
-
-            This.AddOperation(new AccountOperation(
-                -amount,
-                string.Format("Transfer to {0}", destination.Number)));
-
-            destination.AcceptTransferFrom(amount, This.Number);
-        }
-    }
-
-    /// <summary>
-    /// Role of the MoneyTransferContext
-    /// </summary>
-    public class DestinationAccount : RoleFor<Account>
-    {
-        public string Number
-        {
-            get { return This.Number; }
-        }
-
-        public void AcceptTransferFrom(decimal amount, string sourceAccountNumber)
-        {
-            This.AddOperation(new AccountOperation(
-                amount,
-                string.Format("Transfer from {0}", sourceAccountNumber)));
-        }
-    }
-
-    /// <summary>
-    /// Context.
-    /// </summary>
-    public class MoneyTransfer
-    {
-        public decimal Amount;
-        public DestinationAccount Destination;
-        public SourceAccount Source;
-
-        public void Execute()
-        {
-            Source.TransferTo(Amount, Destination);
-        }
-    }
-
-    /// <summary>
-    /// Role of the BalanceInquiry context.
-    /// </summary>
-    public class BalanceInquiryAccount : AccountWithBalance
-    {
-        public void ReportBalance()
-        {
-            Console.WriteLine("Balance of {0}: {1:c}", This.Number, Balance);
-        }
-    }
-
-    /// <summary>
-    /// Context.
-    /// </summary>
-    public class BalanceInquiry
-    {
-        public BalanceInquiryAccount Account;
-
-        public void Execute()
-        {
-            Account.ReportBalance();
-        }
-    }
-
     public static class StaticRolesExample
     {
+        /// <summary>
+        /// The only class of the "composition framework".
+        /// </summary>
+        public abstract class RoleFor<T>
+        {
+            public T This { protected get; set; }
+        }
+
+        /// <summary>
+        /// Dumb data: value object.
+        /// </summary>
+        public class AccountOperation
+        {
+            public AccountOperation(decimal amount, string description)
+            {
+                Amount = amount;
+                Description = description;
+            }
+
+            public decimal Amount { get; private set; }
+
+            public string Description { get; private set; }
+        }
+
+        /// <summary>
+        /// Dumb data: entity.
+        /// </summary>
+        public class Account
+        {
+            private readonly List<AccountOperation> operations = new List<AccountOperation>();
+
+            public Account(string number)
+            {
+                Number = number;
+            }
+
+            public string Number { get; private set; }
+
+            public IEnumerable<AccountOperation> Operations
+            {
+                get { return operations.AsEnumerable(); }
+            }
+
+            public void AddOperation(AccountOperation operation)
+            {
+                operations.Add(operation);
+                Console.WriteLine("{0}: {1:c} {2}",
+                    Number,
+                    operation.Amount,
+                    operation.Description);
+            }
+        }
+
+        /// <summary>
+        /// "Intermediate" role for code reuse.
+        /// </summary>
+        public class AccountWithBalance : RoleFor<Account>
+        {
+            public decimal Balance
+            {
+                get { return This.Operations.Sum(x => x.Amount); }
+            }
+        }
+
+        /// <summary>
+        /// Role of the MoneyTransfer context.
+        /// </summary>
+        public class SourceAccount : AccountWithBalance
+        {
+            public void TransferTo(decimal amount, DestinationAccount destination)
+            {
+                if (Balance < amount)
+                {
+                    throw new ApplicationException("Insufficient funds.");
+                }
+
+                This.AddOperation(new AccountOperation(
+                    -amount,
+                    string.Format("Transfer to {0}", destination.Number)));
+
+                destination.AcceptTransferFrom(amount, This.Number);
+            }
+        }
+
+        /// <summary>
+        /// Role of the MoneyTransferContext
+        /// </summary>
+        public class DestinationAccount : RoleFor<Account>
+        {
+            public string Number
+            {
+                get { return This.Number; }
+            }
+
+            public void AcceptTransferFrom(decimal amount, string sourceAccountNumber)
+            {
+                This.AddOperation(new AccountOperation(
+                    amount,
+                    string.Format("Transfer from {0}", sourceAccountNumber)));
+            }
+        }
+
+        /// <summary>
+        /// Context.
+        /// </summary>
+        public class MoneyTransfer
+        {
+            public decimal Amount;
+            public DestinationAccount Destination;
+            public SourceAccount Source;
+
+            public void Execute()
+            {
+                Source.TransferTo(Amount, Destination);
+            }
+        }
+
+        /// <summary>
+        /// Role of the BalanceInquiry context.
+        /// </summary>
+        public class BalanceInquiryAccount : AccountWithBalance
+        {
+            public void ReportBalance()
+            {
+                Console.WriteLine("Balance of {0}: {1:c}", This.Number, Balance);
+            }
+        }
+
+        /// <summary>
+        /// Context.
+        /// </summary>
+        public class BalanceInquiry
+        {
+            public BalanceInquiryAccount Account;
+
+            public void Execute()
+            {
+                Account.ReportBalance();
+            }
+        }
+
         public static void Run()
         {
             var source = new Account("1001");
