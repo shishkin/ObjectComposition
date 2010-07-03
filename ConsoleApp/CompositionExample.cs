cb0282fe0e44e3918ea65779c6f052b8a30fa2ba
Sergey Shishkin <sergei.shishkin@gmail.com>
2010-07-03 14:54:12 +0200

    Added basic MEF composer

--- ConsoleApp/CompositionExample.cs
+++ ConsoleApp/CompositionExample.cs
@@ -3,9 +3,11 @@ using System.Linq;
 
 namespace DataContextInteraction.CompositionExample
 {
+    using System.Collections.Generic;
     using System.Collections.ObjectModel;
     using System.ComponentModel.Composition;
     using System.ComponentModel.Composition.Hosting;
+    using System.ComponentModel.Composition.Primitives;
     using System.Reflection;
 
     [Export]
@@ -83,7 +85,35 @@ namespace DataContextInteraction.CompositionExample
         }
     }
 
+    public class Composer
+    {
+        private static readonly ComposablePartCatalog Catalog =
+            new AssemblyCatalog(Assembly.GetExecutingAssembly());
 
+        public T Compose<T>(params object[] dependencies)
+        {
+            var container = new CompositionContainer(Catalog);
+            var batch = new CompositionBatch();
+            dependencies
+                .ToList().ForEach(x => AddExportedValue(batch, x));
+            container.Compose(batch);
+            return container.GetExportedValue<T>();
+        }
+
+        /// <summary>
+        /// I wish MEF provided this method out of the box.
+        /// </summary>
+        private static ComposablePart AddExportedValue(CompositionBatch batch, object exportedValue)
+        {
+            var contractName = AttributedModelServices.GetContractName(exportedValue.GetType());
+            var typeIdentity = AttributedModelServices.GetTypeIdentity(exportedValue.GetType());
+            var metadata = new Dictionary<string, object>
+            {
+                { "ExportTypeIdentity", typeIdentity }
+            };
+            return batch.AddExport(new Export(contractName, metadata, () => exportedValue));
+        }
+    }
 
     public class Sample
     {
@@ -91,15 +121,9 @@ namespace DataContextInteraction.CompositionExample
         {
             var entity = new Account(300);
 
-            var catalog = new AssemblyCatalog(Assembly.GetExecutingAssembly());
-            var container = new CompositionContainer(catalog);
-            var batch = new CompositionBatch();
-            batch.AddPart(entity);
-            batch.AddExportedValue(100m);
-            container.Compose(batch);
-            var context = container.GetExportedValue<CashWithdrawal>();
+            var composer = new Composer();
 
-            context.Trigger();
+            composer.Compose<CashWithdrawal>(entity, 120m).Trigger();
 
             entity.Operations.ToList()
                 .ForEach(x => Console.WriteLine("{0:c} {1}", x.Amount, x.Description));
